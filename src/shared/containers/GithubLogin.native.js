8c86a287d4e571bca4430233a9a88ed53beb1f4f
Rahul gaba <rahulgaba16@gmail.com>
2018-05-11 13:12:03 +0530

    Working android integration

--- src/shared/containers/GithubLogin.native.js
+++ src/shared/containers/GithubLogin.native.js
@@ -1,15 +1,15 @@
 import OAuthManager from 'react-native-oauth';
 
-const manager = new OAuthManager('firestackexample')
+const manager = new OAuthManager('githubsearch')
 manager.configure({
   github: {
-    client_id: '1edaa48ff8fc280178a0',
-    client_secret: 'bcdf8828f80c9ba4457184ff4bbbb5b0a7079612'
+    client_id: '551957e1a0313ce3ab27',
+    client_secret: 'ad39cade04054dc7b1e465055f7a34bf92e2398f'
   }
 });
 
 // ...
 manager.authorize('github', { scopes: 'profile email' })
-  .then(resp => console.log('Your users ID'))
+  .then(resp => console.log('Your users ID', resp))
   .catch(err => console.log('There was an error'));
 
