a123f8a4b8ee24ceb4df6d1c41901935edbab47f
Rahul gaba <rahulgaba16@gmail.com>
2018-05-12 18:10:17 +0530

    Removed debugger init steps since it doesn’t work

--- src/mobile/index.js
+++ src/mobile/index.js
@@ -2,14 +2,4 @@ import { AppRegistry } from 'react-native';
 import withStore from 'shared/hocs/withStore';
 import SearchScene from 'shared/scenes/SearchScene';
 
-if(__DEV__) {
-  global.XMLHttpRequest = global.originalXMLHttpRequest ?
-    global.originalXMLHttpRequest :
-    global.XMLHttpRequest;
-  global.FormData = global.originalFormData ?
-    global.originalFormData :
-    global.FormData;
-}
-
-
 AppRegistry.registerComponent('githubsearch', () => withStore(SearchScene));
